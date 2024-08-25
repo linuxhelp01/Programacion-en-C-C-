393b8b432f749fbf14f9c64036a172e178f848fa
linuxhelp01 <felipe.pena2001@alumnos.ubiobio.cl>
2024-08-24 23:15:52 -0400

    Ciclos while y if

---
+++ Ejercicios-propios/Algo-n1.c
@@ -0,0 +1,32 @@
+/* Objetico del programa: Multiplicar a por el valor ingresado y guardado en la variable b.
+*/
+
+#include <stdio.h>
+#define g 10
+
+char b = 0;
+
+int main(void)
+{   
+    while (1) // Para que realice la pregunta una y otra vez
+    {
+        // Pide el valor de b
+        printf("Cuantas veces la grabedad de la tierra desea?\n Esta puede ser maximo de 12 veces la gravedad de la tierra\n");
+        printf("Ingrese valor de b:");                                 
+        scanf("%d",&b);
+
+        // Verifica si la operacion es valida
+        if(b>12)
+        {
+        printf("Error\n"); // Su supera el valor dicho indica error y el programa vuelve al inicio
+        }
+        // Si b es menor o igual que 12 realiza la operacion y muesta la gravedad pedida
+        else
+        {
+            b = b*g;
+            printf("La cantidad de veces que se desea aumentar la grabedad es: %d\n",b);
+        }
+    }
+    
+    return 0;
+}
